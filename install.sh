c2183ca9b9359353021b73ef1153996e54d96da7
Olivier Detour <detour.olivier@gmail.com>
2013-04-15 20:29:34 +0200

    Clean Vim Config.

--- install.sh
+++ install.sh
@@ -6,18 +6,18 @@ RM=rm
 stat_busy "Vim installation"
 
 for dir in backup temp; do
-  if [ ! -d vim/$dir ]; then
-    mkdir vim/$dir
-    printhl "$dir created"
-  fi
+	if [ ! -d vim/$dir ]; then
+		mkdir vim/$dir
+		printhl "$dir created"
+	fi
 done
 
 for file in vimrc vim viminfo; do
-  if [ -e ~/.$file ]; then
-    $RM ~/.$file
-  fi
-  ln -s $PWD/$file ~/.$file
-  printhl "$file linked"
+	if [ -e ~/.$file ]; then
+		$RM ~/.$file
+	fi
+	ln -s $PWD/$file ~/.$file
+	printhl "$file linked"
 done
 
 stat_done
