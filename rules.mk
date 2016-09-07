845f9280e4d52c89e590add0ee0197445edc5e2e
Olivier Detour <detour.olivier@gmail.com>
2016-09-07 15:39:28 +0200

    Remove viminfo from rules.mk

--- rules.mk
+++ rules.mk
@@ -9,9 +9,7 @@ $(_m)-dir-temp		= $(CURDIR)/$(_m)/$(_m)/temp
 
 $(_m)-links =	\
   vimrc		\
-  viminfo	\
   vim
 
 $(_m)-link-vimrc	= ~/.vimrc
-$(_m)-link-viminfo	= ~/.viminfo
 $(_m)-link-vim		= ~/.vim
